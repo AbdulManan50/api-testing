d18e991ba8ab43617f425d11b0b6b887e42d0710
Abdul Manan <abdul.manan@novatoresols.com>
2025-05-29 12:15:13 +0500

    pull ohome

--- src/App.jsx
+++ src/App.jsx
@@ -7,20 +7,11 @@ import { AuthProvider } from "./context/AuthContext";
 import { AuthGuard } from "./Component/AuthGuard";
 import { PublicRoute } from "./Component/PublicRoute";
 import NotFound from "./Page/NotFound";
-<<<<<<< HEAD
-import { Provider } from "react-redux";
-import { store } from "./Store/Store";
-
-export default function App() {
-  return (
-    <Provider store={store}>
-=======
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
 export default function App() {
   return (
     <GoogleOAuthProvider clientId="721269353690-rcbdq1pfon56qdcfmth07ojjq4kq3ap4.apps.googleusercontent.com">
->>>>>>> 1db82aa16d5b63560d560e6dc9f2110c121d94f6
       <AuthProvider>
         <BrowserRouter>
           <Routes>
@@ -52,11 +43,6 @@ export default function App() {
           </Routes>
         </BrowserRouter>
       </AuthProvider>
-<<<<<<< HEAD
-    </Provider>
-
-=======
     </GoogleOAuthProvider>
->>>>>>> 1db82aa16d5b63560d560e6dc9f2110c121d94f6
   );
 }
